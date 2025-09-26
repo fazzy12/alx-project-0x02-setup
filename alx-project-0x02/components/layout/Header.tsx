e3c1dc1af0355cdd6fbcafdd338bf743cca90621
ifeanyi kalu <kaluifeanyi00@gmail.com>
2025-09-26 03:48:17 +0100

    Scaffold and Configure the Project

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import Link from 'next/link';
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-gray-800 text-white p-4">
+      <nav className="container mx-auto flex justify-between items-center">
+        <Link href="/">
+          <a className="text-xl font-bold">ALX Project</a>
+        </Link>
+        <div>
+          <Link href="/home">
+            <a className="mx-2 hover:text-blue-400">Home</a>
+          </Link>
+          <Link href="/about">
+            <a className="mx-2 hover:text-blue-400">About</a>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
